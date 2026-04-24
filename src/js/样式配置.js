ca07a53627679aac5c8872d1fc95654f90e43815
TTQWNTian <TTQWNTian@qq.com>
2026-04-24 14:41:51 +0800

    新增动画.css文件

--- src/js/样式配置.js
+++ src/js/样式配置.js
@@ -3,5 +3,5 @@
 // 角色样式配置
 const 角色样式表 = {
     'Tian': { color: '#ffffff'},
-    'Dvnge': { color: '#FF7F84', fontSize: '1.2em' },
+    'Dvnge': { color: '#FF7F84', animation: '颤抖 0.1s infinite' },
 };
