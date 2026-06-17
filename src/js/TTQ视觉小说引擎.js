d3dd8b1e5a81a81ce4b1c73a0a1e43cc1dc97080
TTQWNTian <TTQWNTian@qq.com>
2026-06-17 19:10:11 +0800

    修改许可证

--- src/js/TTQ视觉小说引擎.js
+++ src/js/TTQ视觉小说引擎.js
@@ -1,3 +1,10 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2025-2026 Tian
+ */
 // js/TTQ视觉小说引擎.js
 // 版本: v1.8.0-dev
 // 开发者: Tian
